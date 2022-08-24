cd5f58c7ae53d1e9d09b016642402e8fe8abe0be
Martin Weise <martin.weise@tuwien.ac.at>
2022-08-24 15:06:24 +0200

    Added Python API

---
+++ .jupyter/api_database/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from api_database.api.container_database_endpoint_api import ContainerDatabaseEndpointApi
+from api_database.api.license_endpoint_api import LicenseEndpointApi
