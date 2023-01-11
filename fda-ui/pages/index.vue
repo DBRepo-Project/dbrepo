f95f06c068f80e5905a7c671700453248a455612
Martin Weise <martin.weise@tuwien.ac.at>
2023-01-11 08:24:09 +0100

    Added new version info

--- fda-ui/pages/index.vue
+++ fda-ui/pages/index.vue
@@ -9,29 +9,19 @@
               <a href="https://dbrepo-docs.ossdip.at/" target="_blank">Online Documentation</a>
             </li>
             <li>
-              <a href="https://gitlab.phaidra.org/fair-data-austria-db-repository/fda-services" target="_blank">Sourcecode Documentation</a>
+              <a href="https://doi.org/10.2218/ijdc.v17i1.825" target="_blank">System Description</a>
+            </li>
+            <li>
+              <a href="https://gitlab.phaidra.org/fair-data-austria-db-repository/fda-services" target="_blank">Sourcecode
+                Documentation</a>
             </li>
             <li>
               <a href="https://hub.docker.com/u/dbrepo" target="_blank">Docker Images</a>
             </li>
             <li>
-              <a href="https://dbrepo.ossdip.at/" target="_blank">Demo Instance</a>
+              <a href="https://dbrepo2.ec.tuwien.ac.at/" target="_blank">Demo Instance</a>
             </li>
           </ul>
-        </div>
-      </v-card-text>
-    </v-card>
-    <v-card flat class="mt-4">
-      <v-card-text>
-        <div>Release Notes</div>
-        <p class="text-h4 text--primary">
-          1.1
-        </p>
-        <p>
-          <a href="https://gitlab.phaidra.org/fair-data-austria-db-repository/fda-services/-/blob/master/CHANGELOG.md">Detailed Changelog</a>
-        </p>
-        <div class="text--primary">
-          In this release we added long-overdue features like user management so each user has their own database (which can also be private) as well as data-insert features directly in the portal.
         </div>
       </v-card-text>
       <v-card-actions>
@@ -42,6 +32,37 @@
           Get Started
         </v-btn>
       </v-card-actions>
+    </v-card>
+    <v-card flat class="mt-4">
+      <v-card-text>
+        <div>Release Notes</div>
+        <p class="text-h4 text--primary">
+          1.2
+        </p>
+        <p>
+          <a href="https://gitlab.phaidra.org/fair-data-austria-db-repository/fda-services/-/blob/master/CHANGELOG.md">Detailed
+            Changelog</a>
+        </p>
+        <div class="text--primary">
+          This release adds semantic information to sub- and supersets, time-versioned metadata database, added access
+          model for giving users access to databases, improved the query store handling.
+        </div>
+      </v-card-text>
+      <v-divider class="mx-4"/>
+      <v-card-text>
+        <div>Release Notes</div>
+        <p class="text-h4 text--primary">
+          1.1
+        </p>
+        <p>
+          <a href="https://gitlab.phaidra.org/fair-data-austria-db-repository/fda-services/-/blob/master/CHANGELOG.md">Detailed
+            Changelog</a>
+        </p>
+        <div class="text--primary">
+          In this release we added long-overdue features like user management so each user has their own database (which
+          can also be private) as well as data-insert features directly in the portal.
+        </div>
+      </v-card-text>
     </v-card>
   </div>
 </template>
