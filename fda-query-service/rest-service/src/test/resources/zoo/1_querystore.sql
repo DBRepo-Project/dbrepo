80676f3192ac07715370c6cdfb5e8805f65f6136
Martin Weise <martin.weise@tuwien.ac.at>
2023-02-08 20:52:46 +0100

    Fixed some pipeline issues

--- fda-query-service/rest-service/src/test/resources/zoo/1_querystore.sql
+++ fda-query-service/rest-service/src/test/resources/zoo/1_querystore.sql
@@ -16,9 +16,9 @@ DELIMITER $$
 CREATE PROCEDURE hash_table(IN name VARCHAR(255), OUT hash VARCHAR(255))
 BEGIN
     DECLARE _sql TEXT;
-    SELECT CONCAT(''SELECT SHA2(GROUP_CONCAT(CONCAT_WS(\''\'','',
-                  GROUP_CONCAT(CONCAT(''`'', column_name, ''`'') ORDER BY column_name),
-                  '') SEPARATOR \'',\''), 256) AS hash FROM `'', name, ''` INTO @hash;'')
+    SELECT CONCAT('SELECT SHA2(GROUP_CONCAT(CONCAT_WS(\'\',',
+                  GROUP_CONCAT(CONCAT('`', column_name, '`') ORDER BY column_name),
+                  ') SEPARATOR \',\'), 256) AS hash FROM `', name, '` INTO @hash;')
     FROM `information_schema`.`columns`
     WHERE `table_schema` = DATABASE()
       AND `table_name` = name
@@ -29,9 +29,9 @@ DELIMITER  $$
 CREATE PROCEDURE store_query(IN query TEXT, IN executed DATETIME, OUT queryId BIGINT)
 BEGIN
     DECLARE _queryhash varchar(255) DEFAULT SHA2(query, 256);
-    DECLARE _username varchar(255) DEFAULT REGEXP_REPLACE(current_user(), ''@.*'', '''');
-    DECLARE _query TEXT DEFAULT CONCAT(''CREATE TABLE IF NOT EXISTS _tmp AS ('', query, '')''); DROP TABLE IF EXISTS _tmp;
-    PREPARE stmt FROM _query; EXECUTE stmt; DEALLOCATE PREPARE stmt; CALL hash_table(''_tmp'', @hash);
+    DECLARE _username varchar(255) DEFAULT REGEXP_REPLACE(current_user(), '@.*', '');
+    DECLARE _query TEXT DEFAULT CONCAT('CREATE TABLE IF NOT EXISTS _tmp AS (', query, ')'); DROP TABLE IF EXISTS _tmp;
+    PREPARE stmt FROM _query; EXECUTE stmt; DEALLOCATE PREPARE stmt; CALL hash_table('_tmp', @hash);
     SELECT COUNT(*) FROM _tmp INTO @count;
     INSERT INTO `qs_queries` (`created_by`, `query`, `query_normalized`, `is_persisted`, `query_hash`, `result_hash`,
                               `result_number`, `executed`)
@@ -41,11 +41,11 @@ BEGIN
 END $$
 DELIMITER  $$
 CREATE
-    DEFINER = ''root'' PROCEDURE _store_query(IN _username VARCHAR(255), IN query TEXT, IN executed DATETIME, OUT queryId BIGINT)
+    DEFINER = 'root' PROCEDURE _store_query(IN _username VARCHAR(255), IN query TEXT, IN executed DATETIME, OUT queryId BIGINT)
 BEGIN
     DECLARE _queryhash varchar(255) DEFAULT SHA2(query, 256);
-    DECLARE _query TEXT DEFAULT CONCAT(''CREATE TABLE IF NOT EXISTS _tmp AS ('', query, '')''); DROP TABLE IF EXISTS _tmp;
-    PREPARE stmt FROM _query; EXECUTE stmt; DEALLOCATE PREPARE stmt; CALL hash_table(''_tmp'', @hash);
+    DECLARE _query TEXT DEFAULT CONCAT('CREATE TABLE IF NOT EXISTS _tmp AS (', query, ')'); DROP TABLE IF EXISTS _tmp;
+    PREPARE stmt FROM _query; EXECUTE stmt; DEALLOCATE PREPARE stmt; CALL hash_table('_tmp', @hash);
     SELECT COUNT(*) FROM _tmp INTO @count;
     INSERT INTO `qs_queries` (`created_by`, `query`, `query_normalized`, `is_persisted`, `query_hash`, `result_hash`,
                               `result_number`, `executed`)
