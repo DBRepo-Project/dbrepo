a57c9519c2744778c1704b2e0135bc28a6ac1cc7
Martin Weise <martin.weise@tuwien.ac.at>
2023-03-08 09:34:05 +0100

    Fixed the test-case finally once and for all

--- fda-query-service/rest-service/src/test/java/at/tuwien/listener/RabbitMqListenerIntegrationTest.java
+++ fda-query-service/rest-service/src/test/java/at/tuwien/listener/RabbitMqListenerIntegrationTest.java
@@ -3,9 +3,6 @@ package at.tuwien.listener;
 import at.tuwien.BaseUnitTest;
 import at.tuwien.api.amqp.ConsumerDto;
 import at.tuwien.config.*;
-import at.tuwien.exception.*;
-import at.tuwien.gateway.BrokerServiceGateway;
-import at.tuwien.listener.impl.RabbitMqListenerImpl;
 import at.tuwien.repository.jpa.*;
 import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
@@ -18,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.IOException;
@@ -25,10 +23,10 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 
 @Log4j2
+@ActiveProfiles(profiles = "junit")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -39,9 +37,6 @@ public class RabbitMqListenerIntegrationTest extends BaseUnitTest {
 
     @MockBean
     private IndexConfig indexConfig;
-
-    @MockBean
-    private BrokerServiceGateway brokerServiceGateway;
 
     @Autowired
     private Channel channel;
@@ -59,9 +54,6 @@ public class RabbitMqListenerIntegrationTest extends BaseUnitTest {
     private TableRepository tableRepository;
 
     @Autowired
-    private RabbitMqListenerImpl rabbitMqListener;
-
-    @Autowired
     private H2Utils h2Utils;
 
     @Autowired
@@ -71,12 +63,14 @@ public class RabbitMqListenerIntegrationTest extends BaseUnitTest {
     private AmqpConfig amqpConfig;
 
     @Rule
-    public Timeout globalTimeout = Timeout.seconds(60);
+    public Timeout globalTimeout = Timeout.seconds(300);
 
     @BeforeAll
     public static void beforeAll() throws InterruptedException {
         afterAll();
+        /* create networks */
         DockerConfig.createAllNetworks();
+        /* create containers */
         DockerConfig.createContainer(null, CONTAINER_BROKER, 15672, CONTAINER_BROKER_ENV);
         DockerConfig.startContainer(CONTAINER_BROKER);
     }
@@ -102,9 +96,10 @@ public class RabbitMqListenerIntegrationTest extends BaseUnitTest {
     }
 
     @Test
-    public void updateConsumers_succeeds() throws AmqpException, IOException, InterruptedException {
+    public void updateConsumers_succeeds() throws IOException, InterruptedException {
 
         /* pre-condition */
+        assertEquals(0, getConsumers().size());
         assertEquals(2, amqpConfig.getAmqpConsumers());
 
         /* mock */
@@ -115,19 +110,23 @@ public class RabbitMqListenerIntegrationTest extends BaseUnitTest {
         channel.queueBind(TABLE_2_QUEUE_NAME, DATABASE_1_EXCHANGE, TABLE_2_ROUTING_KEY);
         channel.queueDeclare(TABLE_3_QUEUE_NAME, true, false, false, null);
         channel.queueBind(TABLE_3_QUEUE_NAME, DATABASE_1_EXCHANGE, TABLE_3_ROUTING_KEY);
-        when(brokerServiceGateway.findAllConsumers())
-                .thenReturn(List.of());
 
         /* test */
-        rabbitMqListener.updateConsumers();
-        Thread.sleep(10 * 1000);
-        final List<ConsumerDto> response = rabbitMqConfig.findAllConsumers()
+        Thread.sleep(30 * 1000) /* wait for scheduled insert */;
+        final List<ConsumerDto> response = getConsumers();
+        final List<ConsumerDto> consumers1 = response.stream().filter(c -> c.getQueue().getName().equals(TABLE_1_QUEUE_NAME)).collect(Collectors.toList());
+        assertEquals(2, consumers1.size());
+        final List<ConsumerDto> consumers2 = response.stream().filter(c -> c.getQueue().getName().equals(TABLE_2_QUEUE_NAME)).collect(Collectors.toList());
+        assertEquals(2, consumers2.size());
+        final List<ConsumerDto> consumers3 = response.stream().filter(c -> c.getQueue().getName().equals(TABLE_3_QUEUE_NAME)).collect(Collectors.toList());
+        assertEquals(2, consumers3.size());
+    }
+
+    private List<ConsumerDto> getConsumers() throws IOException {
+        return rabbitMqConfig.findAllConsumers()
                 .stream()
                 .filter(c -> List.of(TABLE_1_QUEUE_NAME, TABLE_2_QUEUE_NAME, TABLE_3_QUEUE_NAME).contains(c.getQueue().getName()))
                 .collect(Collectors.toList());
-        assertEquals(2, (int) response.stream().filter(c -> c.getQueue().getName().equals(TABLE_1_QUEUE_NAME)).count());
-        assertEquals(2, (int) response.stream().filter(c -> c.getQueue().getName().equals(TABLE_2_QUEUE_NAME)).count());
-        assertEquals(2, (int) response.stream().filter(c -> c.getQueue().getName().equals(TABLE_3_QUEUE_NAME)).count());
     }
 
 }
