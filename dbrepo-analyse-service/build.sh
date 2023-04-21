3272721df20cc3b5f44378c9c36302c5ead72973
Martin Weise <martin.weise@tuwien.ac.at>
2023-04-21 09:57:07 +0200

    Fixed the pyhton tests again

---
+++ dbrepo-analyse-service/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python3 -m venv ./dbrepo-analyse-service/venv
+source ./dbrepo-analyse-service/venv/bin/activate
+pip install -r ./dbrepo-analyse-service/requirements.txt
---
+++ dbrepo-semantics-service/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python3 -m venv ./dbrepo-semantics-service/venv
+source ./dbrepo-semantics-service/venv/bin/activate
+pip install -r ./dbrepo-semantics-service/requirements.txt
