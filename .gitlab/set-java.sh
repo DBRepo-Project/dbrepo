9997b30a37a3db01f8bda0a9d62cc3ffb756ea6f
Martin Weise <martin.weise@tuwien.ac.at>
2023-05-30 21:05:13 +0200

    Removed Java 17

--- .gitlab/set-java.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$1" -eq "17" ]; then
-  echo 'export JAVA_HOME="/usr/lib/jvm/java-17-openjdk/"' > ~/.mavenrc
-elif [ "$1" -eq "11" ]; then
-  echo 'export JAVA_HOME="/usr/lib/jvm/java-11-openjdk/"' > ~/.mavenrc
-fi
