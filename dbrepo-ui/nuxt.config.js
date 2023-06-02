ba46899c56366ee89f891a37fabe88d3bf46c6f1
Martin Weise <martin.weise@tuwien.ac.at>
2023-06-02 12:18:54 +0200

    Removed the deployment scripts, removed the git hash

--- dbrepo-ui/nuxt.config.js
+++ dbrepo-ui/nuxt.config.js
@@ -1,6 +1,6 @@
 import path from 'path'
 import colors from 'vuetify/es5/util/colors'
-import { api, icon, search, clientSecret, title, logo, version, defaultPublisher, doiUrl, baseUrl, gitHash, clientId } from './config'
+import { api, icon, search, clientSecret, title, logo, version, defaultPublisher, doiUrl, baseUrl, clientId } from './config'
 
 const proxy = {}
 
@@ -94,8 +94,7 @@ export default {
     clientSecret,
     defaultPublisher,
     doiUrl,
-    baseUrl,
-    gitHash
+    baseUrl
   },
 
   serverMiddleware: [
