788a712aeb6e1fc6792a29350d86fe30af4374bd
grantnert48 <tobias.grantner@tuwien.ac.at>
2023-08-14 13:04:21 +0200

    Added gateways from identifier service to metadata service

--- dbrepo-metadata-service/services/src/main/java/at/tuwien/gateway/QueryServiceGateway.java
+++ dbrepo-metadata-service/services/src/main/java/at/tuwien/gateway/QueryServiceGateway.java
@@ -1,7 +1,13 @@
 package at.tuwien.gateway;
 
+import at.tuwien.api.database.query.QueryDto;
+import at.tuwien.api.identifier.IdentifierSaveDto;
 import at.tuwien.exception.AmqpException;
+import at.tuwien.exception.QueryNotFoundException;
+import at.tuwien.exception.RemoteUnavailableException;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface QueryServiceGateway {
 
     /**
@@ -13,4 +19,28 @@ public interface QueryServiceGateway {
      */
     void declareConsumer(Long databaseId, Long tableId, String authorization) throws AmqpException;
 
+    /**
+     * Finds a query by given id from the query service that internally looks in the query store of a container.
+     *
+     * @param databaseId    The database id.
+     * @param identifier    The identifier containing the query id and database id.
+     * @param authorization The authorization token.
+     * @return The query information if successful.
+     * @throws QueryNotFoundException     The query was not found.
+     * @throws RemoteUnavailableException The remote service is not available.
+     */
+    QueryDto find(Long databaseId, IdentifierSaveDto identifier, String authorization)
+            throws QueryNotFoundException, RemoteUnavailableException;
+
+    /**
+     * Exports a query by given id.
+     *
+     * @param databaseId  The database id.
+     * @param queryId     The query id.
+     * @return The exported resource as bytes.
+     * @throws RemoteUnavailableException The remote service is not available.
+     * @throws QueryNotFoundException     The query was not found.
+     */
+    byte[] export(Long databaseId, Long queryId) throws RemoteUnavailableException,
+            QueryNotFoundException;
 }
