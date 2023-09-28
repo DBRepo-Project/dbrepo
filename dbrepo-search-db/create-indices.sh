8caa8250b5e40a901321f3f53cf8e22324ffc8cc
Martin Weise <martin.weise@tuwien.ac.at>
2023-09-28 18:21:31 +0200

    Fixed the creation of indices again

--- dbrepo-search-db/create-indices.sh
+++ dbrepo-search-db/create-indices.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-until curl -sSL -o /dev/null 127.0.0.1:9200/_cat/indices 2>&1
+until curl -sSL -o /dev/null localhost:9200/_plugins/_security/api/account -H "Authorization: Basic YWRtaW46YWRtaW4=" 2>&1
 do
   echo "[create-indices.sh] OpenSearch not yet ready"
   sleep 5
 done
 echo "[create-indices.sh] OpenSearch ready"
 for index in "user" "view" "database" "identifier" "concept" "column" "table" "unit"; do
-  RES=$(curl -sSL -X PUT "127.0.0.1:9200/$index" -H "Content-Type: application/json" --data "@$index.json")
+  RES=$(curl -sSL -X PUT "127.0.0.1:9200/$index" -H "Authorization: Basic YWRtaW46YWRtaW4=" -H "Content-Type: application/json" --data "@$index.json")
   ACK=$(echo "$RES" | jq .acknowledged)
   if [ $ACK ]; then
     echo "[create-indices.sh] Created $index index"
