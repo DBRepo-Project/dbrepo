65c01044243282f2ee6884a1378eda60ebcfc9cd
Martin Weise <martin.weise@tuwien.ac.at>
2023-10-30 09:13:27 +0100

    More debug

--- .docs/.swagger/generate.sh
+++ .docs/.swagger/generate.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # clean up
-echo "clean up ./dist ./site"
-rm -rf ./dist ./site
+echo "clean up .docs/.swagger/dist ./site"
+rm -rf .docs/.swagger/dist ./site
 
 # ensure target directories are present
-echo "ensure target directories ./dist ./site are present"
-mkdir -p ./dist ./site
+echo "ensure target directories .docs/.swagger/dist ./site are present"
+mkdir -p .docs/.swagger/dist ./site
 
 # extract static site
-echo "extract static site ./dist.tar.gz"
-tar xzf ./dist.tar.gz
+echo "extract static site .docs/.swagger/dist.tar.gz"
+tar xzf .docs/.swagger/dist.tar.gz
 for service in "analyse" "mirror" "data" "metadata" "upload"; do
   mkdir -p ./site/$service
-  echo "extract static site ./dist -> ./site/$service"
-  cp -r ./dist/* ./site/$service
-  echo "placing ./api-$service.yaml -> ./site/$service/api.yaml"
-  cp "./api-$service.yaml" "./site/$service/api.yaml"
+  echo "extract static site .docs/.swagger/dist -> ./site/$service"
+  cp -r .docs/.swagger/dist/* ./site/$service
+  echo "placing .docs/.swagger/api-$service.yaml -> ./site/$service/api.yaml"
+  cp ".docs/.swagger/api-$service.yaml" "./site/$service/api.yaml"
 done
