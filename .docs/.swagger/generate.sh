04652e156d0b9cc794eedaf5c4e34a06df074f36
Martin Weise <martin.weise@tuwien.ac.at>
2023-10-30 08:41:48 +0100

    Added more stages

--- .docs/.swagger/generate.sh
+++ .docs/.swagger/generate.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+prefix=$1
+
 # clean up
 echo "clean up ./dist ./site"
 rm -rf ./dist ./site
@@ -11,9 +13,9 @@ mkdir -p ./dist ./site
 echo "extract static site ./dist.tar.gz"
 tar xzf ./dist.tar.gz
 for service in "analyse" "mirror" "data" "metadata" "upload"; do
-  mkdir -p ./site/$service
-  echo "extract static site ./dist -> ./site/$service"
-  cp -r ./dist/* ./site/$service
-  echo "placing ./api-$service.yaml -> ./site/$service/api.yaml"
-  cp "./api-$service.yaml" "./site/$service/api.yaml"
+  mkdir -p ./site/$prefix/$service
+  echo "extract static site ./dist -> ./site/$prefix/$service"
+  cp -r ./dist/* ./site/$prefix/$service
+  echo "placing ./api-$service.yaml -> ./site/$prefix/$service/api.yaml"
+  cp "./api-$service.yaml" "./site/$prefix/$service/api.yaml"
 done
