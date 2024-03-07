7770108b22e22f8ec505348bd2d86f65580cad2f
Martin Weise <martin.weise@tuwien.ac.at>
2024-03-07 16:33:48 +0000

    Default better

--- .docs/scripts/extra.js
+++ .docs/scripts/extra.js
@@ -1,12 +1,11 @@
 function getVersion() {
     const segments = location.pathname.split('/');
-    if (segments.length > 4) {
+    if (segments.length > 4 && (segments[3].match(/[0-9]\.[0-9]\.[0-9]/g) || segments[3] == 'latest')) {
         console.debug('version', segments[3]);
         return segments[3];
-    } else {
-        console.debug('default version', '__APPVERSION__');
-        return '__APPVERSION__';
     }
+    console.debug('default version', '__APPVERSION__');
+    return '__APPVERSION__';
 }
 function switchVersion(value) {
     if (!value) { return }
