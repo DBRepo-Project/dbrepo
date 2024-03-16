a61c7c612541456c5a76b623122f21ce8965869a
Martin Weise <martin.weise@tuwien.ac.at>
2024-03-16 21:27:06 +0100

    New attempt in debugging the release version

--- bin/build-docker.sh
+++ bin/build-docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export VERSION=${CI_COMMIT_BRANCH:8:8}
+echo "====> $VERSION"
 docker build --network=host -t dbrepo-metadata-service:build --target build dbrepo-metadata-service
 docker build --network=host -t dbrepo-data-service:build --target build dbrepo-data-service
 docker compose build --parallel
