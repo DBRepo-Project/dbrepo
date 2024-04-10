71217cacf1a572e1bcbcb51e3b39ee43931be0f9
Martin Weise <martin.weise@tuwien.ac.at>
2024-04-10 12:24:55 +0200

    Updated PyPI.rc

--- lib/python/release.sh
+++ lib/python/release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat ${CI_PIPYRC} | base64 -d > .pypirc
+cat "${CI_PIPYRC}" | base64 -d > .pypirc
 python -m twine upload --config-file .pypirc --verbose --repository pypi ./lib/python/dist/dbrepo-*
