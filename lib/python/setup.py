eb4041832158810d43c83ee671d3125f9bdb0049
Martin Weise <martin.weise@tuwien.ac.at>
2024-07-01 17:10:49 +0200

    Updated services

--- lib/python/setup.py
+++ lib/python/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(name="dbrepo",
       version="1.4.5",
       description="A library for communicating with DBRepo",
-      url="https://www.ifs.tuwien.ac.at/infrastructures/dbrepo/1.4.3/",
+      url="https://www.ifs.tuwien.ac.at/infrastructures/dbrepo/1.4.5/",
       author="Martin Weise",
       license="Apache-2.0",
       author_email="martin.weise@tuwien.ac.at",
