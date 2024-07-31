4b49397286919b1c52db682c3d2d26534bb93972
Martin Weise <martin.weise@tuwien.ac.at>
2024-07-31 20:13:56 +0200

    Hotfix default params for Pandas upload

--- lib/python/setup.py
+++ lib/python/setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name="dbrepo",
-      version="1.4.5",
+      version="1.4.6",
       description="A library for communicating with DBRepo",
       url="https://www.ifs.tuwien.ac.at/infrastructures/dbrepo/1.4.5/",
       author="Martin Weise",
