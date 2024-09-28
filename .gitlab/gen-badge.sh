c19fdb1562831ab39a542ecbedecc4a8c584048d
Martin Weise <martin.weise@tuwien.ac.at>
2024-09-28 16:35:35 +0200

    Correct template

--- .gitlab/gen-badge.sh
+++ .gitlab/gen-badge.sh
@@ -5,7 +5,7 @@ anybadge --label pipeline --value "passed" failed=red passed=green canceled=dark
 PIPELINE_COVERAGE=$(curl -fsSL -H "PRIVATE-TOKEN: ${CI_TOKEN}" "${GITLAB_URL}/api/v4/projects/450/pipelines/latest?ref=${CI_COMMIT_BRANCH}" | jq --raw-output .coverage)
 echo "[INFO] pipeline coverage: ${PIPELINE_COVERAGE}"
 if [ "${PIPELINE_COVERAGE}" != "null" ]; then
-  anybadge --label coverage --value "${PIPELINE_COVERAGE}%" 70=red 80=orange 90=yellow 95=green > "./final/${APP_VERSION}/images/coverage.svg"
+  anybadge --label coverage --value "${PIPELINE_COVERAGE}" coverage > "./final/${APP_VERSION}/images/coverage.svg"
 else
   echo "[WARNING] Skipping badge generation, displaying default badge text: unknown"
 fi
