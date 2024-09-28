2cc1771ff312c9fc0b9aaf21033a6d8b591479f5
Martin Weise <martin.weise@tuwien.ac.at>
2024-09-28 14:48:16 +0200

    Updated

--- .gitlab/gen-badge.sh
+++ .gitlab/gen-badge.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 GITLAB_URL="https://gitlab.phaidra.org"
 PIPELINE_STATUS=$(curl -fsSL -H "PRIVATE-TOKEN: ${CI_TOKEN}" "${GITLAB_URL}/api/v4/projects/450/pipelines/latest?ref=${CI_COMMIT_BRANCH}" | jq --raw-output .detailed_status.text)
-anybadge --label pipeline --value "${PIPELINE_STATUS}" failed=red passed=green canceled=darkgray > /tmp/pipeline.svg
+echo "[INFO] pipeline status: ${PIPELINE_STATUS}"
+anybadge --label pipeline --value "${PIPELINE_STATUS}" failed=red passed=green canceled=darkgray > "./final/${APP_VERSION}/images/pipeline.svg"
 PIPELINE_COVERAGE=$(curl -fsSL -H "PRIVATE-TOKEN: ${CI_TOKEN}" "${GITLAB_URL}/api/v4/projects/450/pipelines/latest?ref=${CI_COMMIT_BRANCH}" | jq --raw-output .coverage)
-if [ "${PIPELINE_COVERAGE}" == "null" ]; then
-  PIPELINE_COVERAGE="unknown"
+echo "[INFO] pipeline coverage: ${PIPELINE_COVERAGE}"
+if [ "${PIPELINE_COVERAGE}" != "null" ]; then
+  anybadge --label pipeline --value "${PIPELINE_COVERAGE}" unknown=darkgray 70=red 80=orange 90=yellow 95=green > "./final/${APP_VERSION}/images/coverage.svg"
+else
+  echo "[WARNING] Skipping badge generation, displaying default badge text: unknown"
 fi
-anybadge --label pipeline --value "${PIPELINE_COVERAGE}" unknown=darkgray 70=red 80=orange 90=yellow 95=green > /tmp/coverage.svg
