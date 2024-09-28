29afecc3d70e37418d20cf7146fcae491681152e
Martin Weise <martin.weise@tuwien.ac.at>
2024-09-28 10:29:34 +0200

    Copy -r

--- make/dev.mk
+++ make/dev.mk
@@ -19,8 +19,8 @@ package-config: ## Package the config files
 	cp ./dbrepo-broker-service/advanced.config ./.docker/config
 	cp ./dbrepo-dashboard-service/grafana.ini ./.docker/config
 	cp ./dbrepo-dashboard-service/ldap.toml ./.docker/config
-	cp ./dbrepo-dashboard-service/dashboards ./.docker/config
-	cp ./dbrepo-dashboard-service/provisioning ./.docker/config
+	cp -r ./dbrepo-dashboard-service/dashboards ./.docker/config
+	cp -r ./dbrepo-dashboard-service/provisioning ./.docker/config
 	cp ./dbrepo-gateway-service/dbrepo.conf ./.docker/config
 	cp ./dbrepo-metric-db/prometheus.yml ./.docker/config
 	cp ./dbrepo-storage-service/s3_config.json ./.docker/config
