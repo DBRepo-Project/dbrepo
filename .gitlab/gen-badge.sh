b4c3ab7ca18c36805959939946579bcc05a42fc3
Martin Weise <martin.weise@tuwien.ac.at>
2024-09-28 13:35:40 +0200

    Included coverage for website

--- .gitlab/gen-badge.sh
+++ .gitlab/gen-badge.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 GITLAB_URL="https://gitlab.phaidra.org"
-PIPELINE_ID=$(curl -fsSL -H "PRIVATE-TOKEN: ${CI_TOKEN}" "${GITLAB_URL}/api/v4/projects/450/pipelines?ref=${CI_COMMIT_BRANCH}" | jq '[.[]|select(.)][0].id')
-echo "[INFO] pipeline id: ${PIPELINE_ID}"
-STATUS_RAW=$(curl -fsSL -H "PRIVATE-TOKEN: ${CI_TOKEN}" "${GITLAB_URL}/api/v4/projects/450/pipelines/${PIPELINE_ID}" | jq '.detailed_status')
-echo "[DEBUG] response: ${STATUS_RAW}"
-anybadge --label pipeline --value "$(echo ${STATUS_RAW} | jq --raw-output .text)" failed=red passed=green canceled=darkgray > /tmp/pipeline.svg
+RAW=$(curl -fsSL -H "PRIVATE-TOKEN: ${CI_TOKEN}" "${GITLAB_URL}/api/v4/projects/450/pipelines/latest?ref=${CI_COMMIT_BRANCH}")
+anybadge --label pipeline --value "$(echo ${RAW} | jq --raw-output .detailed_status.text)" failed=red passed=green canceled=darkgray > /tmp/pipeline.svg
+anybadge --label pipeline --value "$(echo ${RAW} | jq --raw-output .coverage)" 70=red 80=orange 90=yellow 95=green > /tmp/coverage.svg
