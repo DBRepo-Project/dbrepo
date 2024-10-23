f4126090f11d5196975b4e1f64beb023f6ed91ef
Martin Weise <martin.weise@tuwien.ac.at>
2024-10-23 13:38:37 +0200

    Wrong version

--- dbrepo-analyse-service/app.py
+++ dbrepo-analyse-service/app.py
@@ -188,7 +188,7 @@ template = {
     "info": {
         "title": "Database Repository Analyse Service API",
         "description": "Service that analyses data structures",
-        "version": "1.4.7",
+        "version": "1.4.6",
         "contact": {
             "name": "Prof. Andreas Rauber",
             "email": "andreas.rauber@tuwien.ac.at"
@@ -200,7 +200,7 @@ template = {
     },
     "externalDocs": {
         "description": "Sourcecode Documentation",
-        "url": "https://www.ifs.tuwien.ac.at/infrastructures/dbrepo/1.4.7/"
+        "url": "https://www.ifs.tuwien.ac.at/infrastructures/dbrepo/1.4.6/"
     },
     "servers": [
         {
--- lib/python/setup.py
+++ lib/python/setup.py
@@ -2,9 +2,9 @@
 from distutils.core import setup
 
 setup(name="dbrepo",
-      version="1.4.7",
+      version="1.4.6",
       description="A library for communicating with DBRepo",
-      url="https://www.ifs.tuwien.ac.at/infrastructures/dbrepo/1.4.7/",
+      url="https://www.ifs.tuwien.ac.at/infrastructures/dbrepo/1.4.6/",
       author="Martin Weise",
       license="Apache-2.0",
       author_email="martin.weise@tuwien.ac.at",
