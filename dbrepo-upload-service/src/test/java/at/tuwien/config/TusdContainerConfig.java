0da8b07d6288c60da20ff5f1d442d5dd7a66f6b8
Martin Weise <martin.weise@tuwien.ac.at>
2024-12-13 13:31:21 +0100

    Hotfix

--- dbrepo-upload-service/src/test/java/at/tuwien/config/TusdContainerConfig.java
+++ dbrepo-upload-service/src/test/java/at/tuwien/config/TusdContainerConfig.java
@@ -1,6 +1,5 @@
 package at.tuwien.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.testcontainers.containers.BindMode;
