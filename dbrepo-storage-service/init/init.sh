c66a4c94b11cedf3534f4fa96025b0111b99ab19
Martin Weise <martin.weise@tuwien.ac.at>
2025-01-02 07:56:32 +0100

    Need different directory for cache

--- dbrepo-storage-service/init/init.sh
+++ dbrepo-storage-service/init/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat << EOF > /app/.s3cfg
+cat << EOF > /app/config/.s3cfg
 access_key = ${S3_ACCESS_KEY_ID}
 secret_key = ${S3_SECRET_ACCESS_KEY}
 # Setup endpoint
@@ -9,4 +9,4 @@ use_https = False
 # Enable S3 v4 signature APIs
 signature_v2 = False
 EOF
-s3cmd --config=/app/.s3cfg mb s3://${S3_BUCKET}
+s3cmd --config=/app/config/.s3cfg mb s3://${S3_BUCKET}
