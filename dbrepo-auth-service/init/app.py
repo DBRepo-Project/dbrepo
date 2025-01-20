73a72bc8c9bd9d1fc91b16e604e419a320d437a5
Martin Weise <martin.weise@tuwien.ac.at>
2025-01-20 12:50:03 +0100

    Hotfix the auth import and bumped charts
    Signed-off-by: Martin Weise <martin.weise@tuwien.ac.at>

--- dbrepo-auth-service/init/app.py
+++ dbrepo-auth-service/init/app.py
@@ -1,34 +1,44 @@
-import logging
 import os
 import sys
 
 import mariadb
-from keycloak import KeycloakAdmin
 
+from requests import post, get
+
+endpoint = os.getenv('AUTH_SERVICE_ENDPOINT', 'http://localhost:8080')
 system_username = os.getenv('SYSTEM_USERNAME', 'admin')
 
-admin = KeycloakAdmin(server_url=os.getenv('AUTH_SERVICE_ENDPOINT', 'http://localhost:8080'),
-                      username=os.getenv('AUTH_SERVICE_ADMIN', 'admin'),
-                      password=os.getenv('AUTH_SERVICE_ADMIN_PASSWORD', 'admin'),
-                      verify=True)
-keycloak_user_id = admin.get_user_id(username=system_username)
-logging.info(f'Successfully fetched keycloak user id: {keycloak_user_id}')
-ldap_user = admin.get_user(user_id=keycloak_user_id)
-if ldap_user is None:
-    logging.error(f'Failed to obtain user')
+print(f'Fetching user id of internal user with username: {system_username}')
+response = post(url=f'{endpoint}/realms/master/protocol/openid-connect/token', data=dict({
+    'username': os.getenv('AUTH_SERVICE_ADMIN', 'admin'),
+    'password': os.getenv('AUTH_SERVICE_ADMIN_PASSWORD', 'admin'),
+    'grant_type': 'password',
+    'client_id': 'admin-cli'
+}))
+
+if response.status_code != 200:
+    print(f'Failed to obtain admin token: {response.status_code}')
+
+response = get(url=f'{endpoint}/admin/realms/dbrepo/users/?username={system_username}', headers=dict({
+    'Authorization': f'Bearer {response.json()["access_token"]}'
+}))
+if len(response.json()) != 1:
+    print(f'Failed to obtain user')
     sys.exit(1)
-ldap_user_attrs = ldap_user.get('attributes')
+ldap_user = response.json()[0]
+print(f'Successfully fetched user id: {ldap_user["id"]}')
+ldap_user_attrs = ldap_user['attributes']
 if ldap_user_attrs is None:
-    logging.error(f'Failed to obtain user attributes')
+    print(f'Failed to obtain user attributes: {ldap_user}')
     sys.exit(1)
 if 'LDAP_ID' not in ldap_user_attrs:
-    logging.error(f'Failed to obtain ldap id: LDAP_ID not in attributes {ldap_user_attrs}')
+    print(f'Failed to obtain ldap id: LDAP_ID not in attributes {ldap_user_attrs}')
     sys.exit(1)
 if len(ldap_user_attrs['LDAP_ID']) != 1:
-    logging.error(f'Failed to obtain ldap id: wrong length {len(ldap_user_attrs["LDAP_ID"])} != 1')
+    print(f'Failed to obtain ldap id: wrong length {len(ldap_user_attrs["LDAP_ID"])} != 1')
     sys.exit(1)
 ldap_user_id = ldap_user_attrs['LDAP_ID'][0]
-logging.info(f'Successfully fetched ldap user id: {ldap_user_id}')
+print(f'Successfully fetched ldap user id: {ldap_user_id}')
 
 try:
     conn = mariadb.connect(user=os.getenv('METADATA_USERNAME', 'root'),
@@ -43,8 +53,8 @@ try:
     conn.commit()
     conn.close()
 except mariadb.Error as e:
-    logging.info(f"Error connecting to MariaDB Platform: {e}")
+    print(f"Error connecting to MariaDB Platform: {e}")
     exit(1)
 
-logging.info(f'Successfully inserted user')
+print(f'Successfully inserted user')
 exit(0)
