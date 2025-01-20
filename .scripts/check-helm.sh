6f2562a8681799cb9f68d8bf34909db0178726fd
Martin Weise <martin.weise@tuwien.ac.at>
2025-01-20 13:14:56 +0100

    WIP
    Signed-off-by: Martin Weise <martin.weise@tuwien.ac.at>

--- .scripts/check-helm.sh
+++ .scripts/check-helm.sh
@@ -4,3 +4,8 @@ if [ $? -ne 0 ]; then
 	echo "!!! Please install the Helm values schema plugin first\n"
 	echo "    https://github.com/losisin/helm-values-schema-json"
 fi
+OUTPUT=$(cat helm/dbrepo/values.yaml | grep "registry.datalab.tuwien.ac.at/.*" | grep -v "$APP_VERSION")
+if [ $? -ne 1 ]; then
+  echo "[ERROR] Some image version(s) differ from APP_VERSION=${APP_VERSION} in Helm Char: ${OUTPUT}"
+  exit 1
+fi
