1329bcb74c7cec1db3b6ed85d6781e591ac5d537
Martin Weise <martin.weise@tuwien.ac.at>
2025-02-17 14:49:24 +0100

    Improved coverage
    Signed-off-by: Martin Weise <martin.weise@tuwien.ac.at>

--- lib/python/tests/test_unit_rest_client.py
+++ lib/python/tests/test_unit_rest_client.py
@@ -1,41 +1,100 @@
 import os
-from unittest import TestCase, mock, main
+import unittest
+
+import requests_mock
 
 from dbrepo.RestClient import RestClient
+from dbrepo.api.dto import JwtAuth
+from dbrepo.api.exceptions import MalformedError, ServiceConnectionError, ServiceError, ForbiddenError, \
+    AuthenticationError, ResponseCodeError
 
 
-class DatabaseUnitTest(TestCase):
+class RestClientUnitTest(unittest.TestCase):
 
     def test_constructor_succeeds(self):
-        # test
-        client = RestClient()
-        self.assertEqual("http://localhost", client.endpoint)
-        self.assertIsNone(client.username)
-        self.assertIsNone(client.password)
-        self.assertTrue(client.secure)
+        with requests_mock.Mocker() as mock:
+            # test
+            os.environ['REST_API_SECURE'] = 'True'
+            response = RestClient()
+            self.assertTrue(response.secure)
 
-    @mock.patch.dict(os.environ, {
-        "REST_API_ENDPOINT": "https://test.dbrepo.tuwien.ac.at",
-        "REST_API_USERNAME": "foo",
-        "REST_API_PASSWORD": "bar",
-        "REST_API_SECURE": "false",
-    })
-    def test_constructor_environment_succeeds(self):
-        # test
-        client = RestClient()
-        self.assertEqual("https://test.dbrepo.tuwien.ac.at", client.endpoint)
-        self.assertEqual("foo", client.username)
-        self.assertEqual("bar", client.password)
-        self.assertFalse(client.secure)
+    def test_get_jwt_auth_empty_succeeds(self):
+        with requests_mock.Mocker() as mock:
+            exp = JwtAuth(access_token='ey123',
+                          refresh_token='ey456',
+                          id_token='ey789',
+                          expires_in=3600,
+                          scope='scope',
+                          token_type='Bearer',
+                          not_before_policy=0,
+                          session_state='session_state',
+                          refresh_expires_in=7200)
+            # mock
+            mock.post('/api/user/token', json=exp.model_dump(), status_code=202)
+            # test
+            response = RestClient().get_jwt_auth()
+            self.assertEqual(exp, response)
 
-    def test_constructor_credentials_succeeds(self):
-        # test
-        client = RestClient(username='admin', password='pass')
-        self.assertEqual("http://localhost", client.endpoint)
-        self.assertEqual('admin', client.username)
-        self.assertEqual('pass', client.password)
-        self.assertTrue(client.secure)
+    def test_get_jwt_auth_400_fails(self):
+        with requests_mock.Mocker() as mock:
+            # mock
+            mock.post('/api/user/token', status_code=400)
+            # test
+            try:
+                response = RestClient().get_jwt_auth()
+            except MalformedError:
+                pass
+
+    def test_get_jwt_auth_403_fails(self):
+        with requests_mock.Mocker() as mock:
+            # mock
+            mock.post('/api/user/token', status_code=403)
+            # test
+            try:
+                response = RestClient().get_jwt_auth()
+            except ForbiddenError:
+                pass
+
+    def test_get_jwt_auth_428_fails(self):
+        with requests_mock.Mocker() as mock:
+            # mock
+            mock.post('/api/user/token', status_code=428)
+            # test
+            try:
+                response = RestClient().get_jwt_auth()
+            except AuthenticationError:
+                pass
+
+    def test_get_jwt_auth_502_fails(self):
+        with requests_mock.Mocker() as mock:
+            # mock
+            mock.post('/api/user/token', status_code=502)
+            # test
+            try:
+                response = RestClient().get_jwt_auth()
+            except ServiceConnectionError:
+                pass
+
+    def test_get_jwt_auth_503_fails(self):
+        with requests_mock.Mocker() as mock:
+            # mock
+            mock.post('/api/user/token', status_code=503)
+            # test
+            try:
+                response = RestClient().get_jwt_auth()
+            except ServiceError:
+                pass
+
+    def test_get_jwt_auth_unknown_fails(self):
+        with requests_mock.Mocker() as mock:
+            # mock
+            mock.post('/api/user/token', status_code=418)
+            # test
+            try:
+                response = RestClient().get_jwt_auth()
+            except ResponseCodeError:
+                pass
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
