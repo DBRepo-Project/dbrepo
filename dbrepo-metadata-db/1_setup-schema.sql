9cd68c576f27acdd8488c209dc6704ba40603884
Martin Weise <martin.weise@tuwien.ac.at>
2025-02-19 19:07:45 +0100

    Wrong unique key
    Signed-off-by: Martin Weise <martin.weise@tuwien.ac.at>

--- dbrepo-metadata-db/1_setup-schema.sql
+++ dbrepo-metadata-db/1_setup-schema.sql
@@ -545,7 +545,7 @@ CREATE TABLE IF NOT EXISTS `mdb_image_operators`
     documentation TEXT            NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (image_id) REFERENCES `mdb_images` (`id`),
-    UNIQUE (value)
+    UNIQUE (image_id, value)
 ) WITH SYSTEM VERSIONING;
 
 COMMIT;
