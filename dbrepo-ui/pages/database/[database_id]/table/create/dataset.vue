665d2d3235f292f1bacb9fcaa01acf43537e37ab
Martin Weise <martin.weise@tuwien.ac.at>
2025-04-23 19:25:45 +0200

    Updated the primary key composite
    Signed-off-by: Martin Weise <martin.weise@tuwien.ac.at>

--- dbrepo-ui/pages/database/[database_id]/table/create/dataset.vue
+++ dbrepo-ui/pages/database/[database_id]/table/create/dataset.vue
@@ -155,12 +155,11 @@
             </v-stepper-header>
             <v-stepper-window
               direction="vertical">
-              <v-container
-                v-if="step >= 4">
+              <v-container>
                 <TableSchema
                   ref="schema"
                   :back="false"
-                  :disabled="step > 4"
+                  :disabled="step !== 4"
                   :loading="loading"
                   :submit-text="$t('navigation.continue')"
                   :columns="tableCreate.columns"
