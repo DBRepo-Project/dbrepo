c362823a680f33151cb811071d24e203c40a8fcf
Martin Weise <martin.weise@tuwien.ac.at>
2025-05-30 21:36:08 +0200

    WIP
    Signed-off-by: Martin Weise <martin.weise@tuwien.ac.at>

--- .gitlab/gen-badge.sh
+++ .gitlab/gen-badge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 anybadge --label pipeline --value "passed" failed=red passed=green canceled=darkgray > "./final/${DOC_VERSION}/images/pipeline.svg"
 
-URL = "${GITLAB_URL}/api/v4/projects/${GITLAB_PROJECT_ID}/pipelines/latest?ref=${CI_COMMIT_BRANCH}"
+URL="${GITLAB_URL}/api/v4/projects/${GITLAB_PROJECT_ID}/pipelines/latest?ref=${CI_COMMIT_BRANCH}"
 echo "[DEBUG] obtaining pipeline coverage from url: $URL ..."
 PIPELINE_COVERAGE=$(curl -fsSL -H "PRIVATE-TOKEN: ${CI_TOKEN}" $URL | jq --raw-output .coverage)
 echo "[INFO] pipeline coverage: ${PIPELINE_COVERAGE}"
@@ -13,11 +13,11 @@ else
 fi
 
 
-URL = "${CI_SONAR_URL}/api/project_badges/measure?project=${CI_SONAR_PROJECT_KEY}&metric=sqale_rating&token=${CI_SONAR_BADGE_TOKEN}"
+URL="${CI_SONAR_URL}/api/project_badges/measure?project=${CI_SONAR_PROJECT_KEY}&metric=sqale_rating&token=${CI_SONAR_BADGE_TOKEN}"
 echo "[DEBUG] obtain maintainability project badge from url: $URL ..."
 curl -fsSL $URL > "./final/${DOC_VERSION}/images/maintainability.svg"
 
-URL = "${CI_SONAR_URL}/api/project_badges/measure?project=${CI_SONAR_PROJECT_KEY}&metric=security_rating&token=${CI_SONAR_BADGE_TOKEN}"
+URL="${CI_SONAR_URL}/api/project_badges/measure?project=${CI_SONAR_PROJECT_KEY}&metric=security_rating&token=${CI_SONAR_BADGE_TOKEN}"
 echo "[DEBUG] obtain security project badge from url: $URL ..."
 curl -fsSL $URL > "./final/${DOC_VERSION}/images/security.svg"
 
