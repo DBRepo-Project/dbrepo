979633cd073b6827eb697bfb9573546b5afa92dc
Martin Weise <martin.weise@tuwien.ac.at>
2025-05-30 07:34:41 +0200

    WIP
    Signed-off-by: Martin Weise <martin.weise@tuwien.ac.at>

--- dbrepo-metadata-service/rest-service/src/test/java/at/ac/tuwien/ifs/dbrepo/config/RabbitConfig.java
+++ dbrepo-metadata-service/rest-service/src/test/java/at/ac/tuwien/ifs/dbrepo/config/RabbitConfig.java
@@ -4,7 +4,14 @@ import at.ac.tuwien.ifs.dbrepo.core.test.BaseTest;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+import java.io.IOException;
 
 @Getter
 @Slf4j
@@ -22,5 +29,15 @@ public class RabbitConfig extends BaseTest {
 
     @Value("${dbrepo.endpoints.brokerService}")
     private String brokerEndpoint;
+
+    @Bean
+    @Primary
+    public RestTemplate brokerRestTemplate() {
+        final RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(brokerEndpoint));
+        restTemplate.getInterceptors()
+                .add(new BasicAuthenticationInterceptor(USER_1_USERNAME, USER_1_PASSWORD));
+        return restTemplate;
+    }
 
 }
