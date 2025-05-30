5259de1ab9eb7d97c3aff7bd1aa38e2a944b5940
Martin Weise <martin.weise@tuwien.ac.at>
2025-05-30 13:07:29 +0200

    Forgot version
    Signed-off-by: Martin Weise <martin.weise@tuwien.ac.at>

--- .gitlab/gen-badge.sh
+++ .gitlab/gen-badge.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 anybadge --label pipeline --value "passed" failed=red passed=green canceled=darkgray > "./final/${DOC_VERSION}/images/pipeline.svg"
-PIPELINE_COVERAGE=$(curl -fsSL -H "PRIVATE-TOKEN: ${CI_TOKEN}" "${GITLAB_URL}/api/v4/projects/${GITLAB_PROJECT_ID}/pipelines/latest?ref=${CI_COMMIT_BRANCH}" | jq --raw-output .coverage)
 
+URL = "${GITLAB_URL}/api/v4/projects/${GITLAB_PROJECT_ID}/pipelines/latest?ref=${CI_COMMIT_BRANCH}"
+echo "[DEBUG] obtaining pipeline coverage from url: $URL ..."
+PIPELINE_COVERAGE=$(curl -fsSL -H "PRIVATE-TOKEN: ${CI_TOKEN}" $URL | jq --raw-output .coverage)
 echo "[INFO] pipeline coverage: ${PIPELINE_COVERAGE}"
+
 if [ "${PIPELINE_COVERAGE}" != "null" ]; then
   anybadge --label coverage --value "${PIPELINE_COVERAGE}" coverage > "./final/${DOC_VERSION}/images/coverage.svg"
 else
   echo "[WARNING] Skipping badge generation, displaying default badge text: unknown"
 fi
 
-curl -fsSL "${CI_SONAR_URL}/api/project_badges/measure?project=${CI_SONAR_PROJECT_KEY}&metric=sqale_rating&token=${CI_SONAR_BADGE_TOKEN}" > "./final/${DOC_VERSION}/images/maintainability.svg"
-curl -fsSL "${CI_SONAR_URL}/api/project_badges/measure?project=${CI_SONAR_PROJECT_KEY}&metric=security_rating&token=${CI_SONAR_BADGE_TOKEN}" > "./final/${DOC_VERSION}/images/security.svg"
+
+URL = "${CI_SONAR_URL}/api/project_badges/measure?project=${CI_SONAR_PROJECT_KEY}&metric=sqale_rating&token=${CI_SONAR_BADGE_TOKEN}"
+echo "[DEBUG] obtain maintainability project badge from url: $URL ..."
+curl -fsSL $URL > "./final/${DOC_VERSION}/images/maintainability.svg"
+
+URL = "${CI_SONAR_URL}/api/project_badges/measure?project=${CI_SONAR_PROJECT_KEY}&metric=security_rating&token=${CI_SONAR_BADGE_TOKEN}"
+echo "[DEBUG] obtain security project badge from url: $URL ..."
+curl -fsSL $URL > "./final/${DOC_VERSION}/images/security.svg"
 
 echo "[INFO] retrieved badges"
