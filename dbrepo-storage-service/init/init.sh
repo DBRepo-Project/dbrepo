a0b7f79436dd40b9a68c211d87438e5539c109be
Martin Weise <martin.weise@tuwien.ac.at>
2025-06-04 15:56:34 +0200

    The chart lacks support for the setup job path
    Signed-off-by: Martin Weise <martin.weise@tuwien.ac.at>

--- dbrepo-storage-service/init/init.sh
+++ dbrepo-storage-service/init/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat << EOF > /app/.s3cfg
+cat << EOF > /app/config/.s3cfg
 access_key = ${S3_ACCESS_KEY_ID}
 secret_key = ${S3_SECRET_ACCESS_KEY}
 # Setup endpoint
@@ -15,15 +15,15 @@ function log() {
 }
 
 # create bucket
-if s3cmd --config=/app/.s3cfg ls | grep -q "s3://${S3_BUCKET}"; then
+if s3cmd --config=/app/config/.s3cfg ls | grep -q "s3://${S3_BUCKET}"; then
   echo "[INFO] Bucket s3://${S3_BUCKET} already exists, skip"
   log "INFO" "Bucket s3://${S3_BUCKET} already exists, skip"
 else
-  if ! s3cmd --config=/app/.s3cfg mb s3://${S3_BUCKET}; then
+  if ! s3cmd --config=/app/config/.s3cfg mb s3://${S3_BUCKET}; then
     echo "[ERROR] Failed to create bucket s3://${S3_BUCKET}"
     log "ERROR" "Failed to create bucket s3://${S3_BUCKET}"
   fi
 fi
 
 # expire daily
-s3cmd --config=/app/.s3cfg expire s3://${S3_BUCKET} --expiry-prefix "" --expiry-days 1
+s3cmd --config=/app/config/.s3cfg expire s3://${S3_BUCKET} --expiry-prefix "" --expiry-days 1
