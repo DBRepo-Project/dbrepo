5131800b2c102189518426a4ee7e5466ba0aedf0
Martin Weise <martin.weise@tuwien.ac.at>
2025-06-07 16:12:56 +0200

    Merge branch release-1.9
    Signed-off-by: Martin Weise <martin.weise@tuwien.ac.at>

--- lib/python/build-site.sh
+++ lib/python/build-site.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-#python3 -m venv ./lib/python/venv
-#source ./lib/python/venv/bin/activate
-#PIPENV_PIPFILE=./lib/python/Pipfile pipenv install --dev
-#sphinx-apidoc -o ./lib/python/docs/source ./lib/python/dbrepo
-#sphinx-build -M html ./lib/python/docs/ ./lib/python/docs/build/
+echo "[DEBUG] Build api docs ..."
+sphinx-apidoc -o ./lib/python/docs/source ./lib/python/dbrepo
 
 echo "[DEBUG] Build markdown docs ..."
 sphinx-build -M markdown ./lib/python/docs/ ./lib/python/docs/build/
@@ -15,3 +11,6 @@ cat ./lib/python/docs/build/markdown/guide/rest-client.md >> ./docs/api/python.m
 
 echo "[DEBUG] Parsing python.tpl docs ..."
 cat ./lib/python/docs/build/markdown/guide/amqp-client.md >> ./docs/api/python.md
+
+echo "[DEBUG] Build html docs ..."
+sphinx-build -M html ./lib/python/docs/ ./lib/python/docs/build/
