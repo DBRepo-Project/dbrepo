084adbeb145a96112b3e83707edbcd65c4ca6123
Martin Weise <martin.weise@tuwien.ac.at>
2025-07-18 16:39:28 +0200

    WIP
    Signed-off-by: Martin Weise <martin.weise@tuwien.ac.at>

---
+++ dbrepo-ui/pages/policies.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-card
+      rounded="0"
+      variant="flat">
+      <v-card-text>
+        <Markdown
+          :source="source" />
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import Markdown from 'vue3-markdown-it'
+import source from '~/content/about.md?raw'
+
+export default {
+  components: {
+    Markdown
+  },
+  data() {
+    return {
+      source: source
+    }
+  }
+}
+</script>
