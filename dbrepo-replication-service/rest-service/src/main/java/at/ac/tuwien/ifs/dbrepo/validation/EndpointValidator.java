217222c662b8cc7bf185f49520d9a523c9ed3136
maxlastglance <e52004266@student.tuwien.ac.at>
2025-07-23 12:35:16 +0200

    recreate rest-service folder from data-service

---
+++ dbrepo-replication-service/rest-service/src/main/java/at/ac/tuwien/ifs/dbrepo/validation/EndpointValidator.java
@@ -0,0 +1,92 @@
+package at.ac.tuwien.ifs.dbrepo.validation;
+
+import at.ac.tuwien.ifs.dbrepo.core.api.database.AccessTypeDto;
+import at.ac.tuwien.ifs.dbrepo.core.api.database.DatabaseAccessDto;
+import at.ac.tuwien.ifs.dbrepo.core.api.database.DatabaseDto;
+import at.ac.tuwien.ifs.dbrepo.core.api.database.query.FilterDto;
+import at.ac.tuwien.ifs.dbrepo.core.api.database.query.FilterTypeDto;
+import at.ac.tuwien.ifs.dbrepo.core.api.database.query.SubsetDto;
+import at.ac.tuwien.ifs.dbrepo.core.exception.*;
+import at.ac.tuwien.ifs.dbrepo.endpoints.RestEndpoint;
+import at.ac.tuwien.ifs.dbrepo.service.CacheService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.security.Principal;
+import java.util.List;
+
+@Slf4j
+@Component
+public class EndpointValidator extends RestEndpoint {
+
+    private final CacheService credentialService;
+
+    @Autowired
+    public EndpointValidator(CacheService credentialService) {
+        this.credentialService = credentialService;
+    }
+
+    public void validateDataParams(Long page, Long size) throws PaginationException {
+        log.trace("validate data params, page={}, size={}", page, size);
+        if ((page == null && size != null) || (page != null && size == null)) {
+            log.error("Failed to validate page and/or size number, either both are present or none");
+            throw new PaginationException("Failed to validate page and/or size number");
+        }
+        if (page != null && page < 0) {
+            log.error("Failed to validate page number, is lower than zero");
+            throw new PaginationException("Failed to validate page number");
+        }
+        if (size != null && size <= 0) {
+            log.error("Failed to validate size number, is lower or equal than zero");
+            throw new PaginationException("Failed to validate size number");
+        }
+    }
+
+    public void validateSubsetParams(SubsetDto subset) throws QueryMalformedException {
+        if (subset.getFilter() != null) {
+            final List<FilterDto> filters = subset.getFilter();
+            FilterTypeDto previous = null;
+            for (int i = 0; i < filters.size(); i++) {
+                final FilterDto filter = filters.get(i);
+                if ((i == 0 && !filter.getType().equals(FilterTypeDto.WHERE)) ||
+                        (i > 0 && !previous.equals(FilterTypeDto.WHERE) && (filter.getType().equals(FilterTypeDto.AND) || filter.getType().equals(FilterTypeDto.OR)))) {
+                    log.error("Failed to validate subset: invalid specification, must be where-[(and|or)-where]");
+                    throw new QueryMalformedException("Failed to validate subset: invalid specification, must be where-[(and|or)-where]");
+                }
+                previous = filter.getType();
+            }
+        }
+    }
+
+    public void validateOnlyAccess(DatabaseDto database, Principal principal, boolean writeAccessOnly)
+            throws NotAllowedException, RemoteUnavailableException, MetadataServiceException {
+        if (principal == null) {
+            throw new NotAllowedException("No principal provided");
+        }
+        if (isSystem(principal)) {
+            return;
+        }
+        final DatabaseAccessDto access = credentialService.getAccess(database.getId(), getUsername(principal));
+        log.trace("found access: {}", access);
+        if (writeAccessOnly && !(access.getType().equals(AccessTypeDto.WRITE_OWN) || access.getType().equals(AccessTypeDto.WRITE_ALL))) {
+            log.error("Access not allowed: no write access");
+            throw new NotAllowedException("Access not allowed: no write access");
+        }
+    }
+
+    public void validateOnlyWriteOwnOrWriteAllAccess(AccessTypeDto access, String owner, String username)
+            throws NotAllowedException {
+        if (access.equals(AccessTypeDto.READ)) {
+            log.error("Failed to create table data: no write access");
+            throw new NotAllowedException("Failed to create table data: no write access");
+        }
+        if (access.equals(AccessTypeDto.WRITE_OWN) && !owner.equals(username)) {
+            log.error("Failed to create table data: insufficient table write access");
+            throw new NotAllowedException("Failed to create table data: insufficient table write access");
+        }
+        log.trace("sufficient write access {} for username {} and owner {}", access, username, owner);
+    }
+
+
+}
---
+++ dbrepo-replication-service/rest-service/src/test/resources/init/weather.sql
@@ -0,0 +1,103 @@
+/* https://www.kaggle.com/jsphyg/weather-dataset-rattle-package */
+CREATE
+DATABASE weather;
+USE
+weather;
+
+CREATE TABLE weather_location
+(
+    location VARCHAR(255) PRIMARY KEY,
+    lat      DOUBLE PRECISION NULL,
+    lng      DOUBLE PRECISION NULL
+) WITH SYSTEM VERSIONING;
+
+CREATE TABLE weather_aus
+(
+    id       SERIAL PRIMARY KEY,
+    `date`   DATE NOT NULL,
+    location VARCHAR(255) NULL COMMENT 'Closest city',
+    mintemp  DOUBLE PRECISION NULL,
+    rainfall DOUBLE PRECISION NULL,
+    FOREIGN KEY (location) REFERENCES weather_location (location) ON DELETE SET NULL,
+    CONSTRAINT some_constraint UNIQUE (`date`),
+    CHECK (`mintemp` > 0)
+) WITH SYSTEM VERSIONING COMMENT 'Weather in Australia';
+
+CREATE TABLE complex_primary_key
+(
+    id       BIGINT NOT NULL,
+    other_id BIGINT NOT NULL,
+    PRIMARY KEY (id, other_id)
+) WITH SYSTEM VERSIONING;
+
+CREATE TABLE complex_foreign_keys
+(
+    id         BIGINT NOT NULL PRIMARY KEY,
+    weather_id BIGINT NOT NULL,
+    other_id   BIGINT NOT NULL,
+    FOREIGN KEY (weather_id, other_id) REFERENCES complex_primary_key (id, `other_id`)
+) WITH SYSTEM VERSIONING;
+
+CREATE TABLE sensor
+(
+    `timestamp` TIMESTAMP NOT NULL PRIMARY KEY,
+    `value`     DECIMAL
+) WITH SYSTEM VERSIONING;
+
+CREATE TABLE exotic_boolean
+(
+    `bool_default`          BOOLEAN NOT NULL PRIMARY KEY,
+    `bool_tinyint`          TINYINT(1)          NOT NULL,
+    `bool_tinyint_unsigned` TINYINT(1) UNSIGNED NOT NULL
+) WITH SYSTEM VERSIONING;
+
+INSERT INTO weather_location (location, lat, lng)
+VALUES ('Albury', -36.0653583, 146.9112214),
+       ('Sydney', -33.847927, 150.6517942),
+       ('Vienna', null, null);
+
+INSERT INTO weather_aus (id, `date`, location, mintemp, rainfall)
+VALUES (1, '2008-12-01', 'Albury', 13.4, 0.6),
+       (2, '2008-12-02', 'Albury', 7.4, 0),
+       (3, '2008-12-03', 'Albury', 12.9, 0);
+
+INSERT INTO sensor (`timestamp`, value)
+VALUES ('2022-12-24 17:00:00', 10.0),
+       ('2022-12-24 18:00:00', 10.2),
+       ('2022-12-24 19:00:00', null),
+       ('2022-12-24 20:00:00', 10.3),
+       ('2022-12-24 21:00:00', 10.0),
+       ('2022-12-24 22:00:00', null);
+
+-- #####################################################################################################################
+-- ## TEST CASE PRE-REQUISITE                                                                                         ##
+-- #####################################################################################################################
+
+CREATE VIEW junit2 AS
+(
+select `date`, `location` as loc, `mintemp`, `rainfall`
+from `weather_aus`
+where `location` = 'Albury');
+
+CREATE TABLE not_in_metadata_db
+(
+    id          BIGINT       NOT NULL PRIMARY KEY,
+    given_name  VARCHAR(255) NOT NULL,
+    middle_name VARCHAR(255) NULL,
+    family_name VARCHAR(255) NOT NULL,
+    age         INT          NOT NULL CHECK ( age > 0 AND age < 120 ),
+    UNIQUE (given_name, family_name)
+) WITH SYSTEM VERSIONING;
+
+CREATE VIEW not_in_metadata_db2 AS
+(
+select `date`, `location`, `mintemp` as `MinTemp`, `rainfall` as `Rainfall`
+from `weather_aus`
+where `location` = 'Vienna');
+
+-- internal view who should not be indexed into schema
+CREATE VIEW 5c7ba02f681b889892ee82987aa6c74ce45a30973cfef06b78ce797f25189b9a AS
+(
+select `date`, `location`, `mintemp` as `MinTemp`, `rainfall` as `Rainfall`
+from `weather_aus`
+where `location` = 'Vienna');
