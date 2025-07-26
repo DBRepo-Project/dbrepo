8ca68ef1e11e9f9dca45bb462de0eb9979f116e2
maxlastglance <e52004266@student.tuwien.ac.at>
2025-07-26 15:48:07 +0200

    add annotations and fix filename

--- lib/java/dbrepo-core/src/main/java/at/ac/tuwien/ifs/dbrepo/core/api/replication/TupleNotificationDto.java
+++ lib/java/dbrepo-core/src/main/java/at/ac/tuwien/ifs/dbrepo/core/api/replication/TupleNotificationDto.java
@@ -2,6 +2,20 @@ package at.ac.tuwien.ifs.dbrepo.core.api.replication;
 
 import java.util.Map;
 
+    
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.extern.jackson.Jacksonized;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Jacksonized
+@ToString
 public class TupleNotificationDto {
     private String tableName;
     private Map<String, Object> tupleData;
