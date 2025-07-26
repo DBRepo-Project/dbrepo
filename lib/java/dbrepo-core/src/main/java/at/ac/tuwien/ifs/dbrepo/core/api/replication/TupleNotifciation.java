dca9980eb4a00e86aa7970d4cbfd9b7b4db79fca
maxlastglance <e52004266@student.tuwien.ac.at>
2025-07-26 15:41:30 +0200

    add package

---
+++ lib/java/dbrepo-core/src/main/java/at/ac/tuwien/ifs/dbrepo/core/api/replication/TupleNotifciation.java
@@ -0,0 +1,18 @@
+package at.ac.tuwien.ifs.dbrepo.core.api.replication;
+
+import java.util.Map;
+
+public class TupleNotificationDto {
+    private String tableName;
+    private Map<String, Object> tupleData;
+    private String timestamp;
+
+    public String getTableName() { return tableName; }
+    public void setTableName(String tableName) { this.tableName = tableName; }
+
+    public Map<String, Object> getTupleData() { return tupleData; }
+    public void setTupleData(Map<String, Object> tupleData) { this.tupleData = tupleData; }
+
+    public String getTimestamp() { return timestamp; }
+    public void setTimestamp(String timestamp) { this.timestamp = timestamp; }
+} 
