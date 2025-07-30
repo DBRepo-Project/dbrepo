4644c234d93344f46fc7fbf5a0c7ad33c37f13d3
maxlastglance <e52004266@student.tuwien.ac.at>
2025-07-30 17:43:46 +0200

    fix endpoint again again

--- dbrepo-replication-service/rest-service/src/main/java/at/ac/tuwien/ifs/dbrepo/endpoints/DatabaseEndpoint.java
+++ dbrepo-replication-service/rest-service/src/main/java/at/ac/tuwien/ifs/dbrepo/endpoints/DatabaseEndpoint.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/database")
+@RequestMapping("/api/replication/api/v1/database")
 @RequiredArgsConstructor
 @Tag(name = "Database", description = "Database replication endpoints")
 public class DatabaseEndpoint {
