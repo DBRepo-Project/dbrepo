8d2fbe01ccc4a30c2b8b0050552bd6a08068b2c7
maxlastglance <e52004266@student.tuwien.ac.at>
2025-07-30 18:06:01 +0200

    fix endpoint and replication rest template

--- dbrepo-metadata-service/services/src/main/java/at/ac/tuwien/ifs/dbrepo/service/impl/ReplicationServiceImpl.java
+++ dbrepo-metadata-service/services/src/main/java/at/ac/tuwien/ifs/dbrepo/service/impl/ReplicationServiceImpl.java
@@ -48,7 +48,7 @@ public class ReplicationServiceImpl implements ReplicationService {
 
             // Send POST request to replication service
             ResponseEntity<Void> response = replicationRestTemplate.exchange(
-                    "/database",
+                    "api/replication/database",
                     HttpMethod.POST,
                     new HttpEntity<>(notificationDto),
                     Void.class
--- dbrepo-replication-service/rest-service/src/main/java/at/ac/tuwien/ifs/dbrepo/endpoints/DatabaseEndpoint.java
+++ dbrepo-replication-service/rest-service/src/main/java/at/ac/tuwien/ifs/dbrepo/endpoints/DatabaseEndpoint.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/database")
+@RequestMapping("api/replication/database")
 @RequiredArgsConstructor
 @Tag(name = "Database", description = "Database replication endpoints")
 public class DatabaseEndpoint {
