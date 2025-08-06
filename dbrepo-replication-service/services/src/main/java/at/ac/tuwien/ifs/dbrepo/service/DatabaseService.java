c50ede7c3f7fd4701c9b61661877a7c17a2a7619
maxlastglance <e52004266@student.tuwien.ac.at>
2025-08-06 09:44:14 +0200

    rm unused imports

--- dbrepo-replication-service/services/src/main/java/at/ac/tuwien/ifs/dbrepo/service/DatabaseService.java
+++ dbrepo-replication-service/services/src/main/java/at/ac/tuwien/ifs/dbrepo/service/DatabaseService.java
@@ -1,15 +1,6 @@
 package at.ac.tuwien.ifs.dbrepo.service;
 
-import at.ac.tuwien.ifs.dbrepo.core.api.database.DatabaseDto;
-import at.ac.tuwien.ifs.dbrepo.core.api.database.ViewDto;
-import at.ac.tuwien.ifs.dbrepo.core.api.database.table.TableDto;
-import at.ac.tuwien.ifs.dbrepo.core.api.database.table.internal.TableCreateDto;
 import at.ac.tuwien.ifs.dbrepo.core.api.replication.DatabaseNotificationDto;
-import at.ac.tuwien.ifs.dbrepo.core.api.user.internal.UpdateUserPasswordDto;
-import at.ac.tuwien.ifs.dbrepo.core.exception.*;
-
-import java.sql.SQLException;
-import java.util.List;
 
 public interface DatabaseService {
 
