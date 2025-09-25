c8716abe04838f085ae8cf29c29309713563b5e1
Martin Weise <martin.weise@tuwien.ac.at>
2025-09-25 20:13:28 +0000

    WIP

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 .PHONY: all
 
-DOC_VERSION ?= 1.10
-APP_VERSION ?= 1.10.5
-CHART_VERSION ?= 1.10.5
+DOC_VERSION ?= 1.11
+APP_VERSION ?= 1.11.0
+CHART_VERSION ?= 1.11.0
 REPOSITORY_URL ?= registry.datalab.tuwien.ac.at/dbrepo
 
 .PHONY: all
--- lib/python/setup.py
+++ lib/python/setup.py
@@ -2,8 +2,8 @@
 import os
 from distutils.core import setup
 
-app_version = os.environ.get("APP_VERSION", "1.10.5")
-doc_version = os.environ.get("DOC_VERSION", "1.10")
+app_version = os.environ.get("APP_VERSION", "1.11.0")
+doc_version = os.environ.get("DOC_VERSION", "1.11")
 
 setup(name="dbrepo",
       version=str(app_version),
