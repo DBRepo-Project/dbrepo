8f1c5a737dd937686ffea60e64af1a22d02b5aed
Martin Weise <martin.weise@tuwien.ac.at>
2025-09-25 21:50:57 +0200

    Bumped version
    Signed-off-by: Martin Weise <martin.weise@tuwien.ac.at>

--- dbrepo-search-service/app.py
+++ dbrepo-search-service/app.py
@@ -183,7 +183,7 @@ template = {
     "info": {
         "title": "Database Repository Search Service API",
         "description": "Service that searches the search database",
-        "version": "1.10.0",
+        "version": "1.11.0",
         "contact": {
             "name": "Prof. Andreas Rauber",
             "email": "andreas.rauber@tuwien.ac.at"
