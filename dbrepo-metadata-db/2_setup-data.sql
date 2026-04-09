cb5f80bc0872780aaeca1b76c7a4d1bfef5f119d
Martin Weise <martin.weise@tuwien.ac.at>
2026-04-09 21:27:45 +0200

    WIP
    Signed-off-by: Martin Weise <martin.weise@tuwien.ac.at>

--- dbrepo-metadata-db/2_setup-data.sql
+++ dbrepo-metadata-db/2_setup-data.sql
@@ -6,7 +6,7 @@ USER readonly WITH PASSWORD 'readonly';
 INSERT INTO dbrepo.mdb_containers (id, name, internal_name, image_id, host, port, ui_host, ui_port, privileged_username,
                                    privileged_password, readonly_username, readonly_password)
 VALUES ('ee960ff5-cca2-4222-bd6f-0f29ac2e1959', 'postgres:17-alpine', 'postgres_17_alpine',
-        '32c13903-651a-404c-8fd3-f92708899a69', 'data-db', 5432, 'localhost', 5432, 'postgres', 'dbrepo', 'readonly',
+        '32c13903-651a-404c-8fd3-f92708899a69', 'data-db', 5432, 'localhost', 5432, 'postgres', 'postgres', 'readonly',
         'readonly');
 
 COMMIT;
