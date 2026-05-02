3c0aee892306af647a90d16830eb46b57becfcaf
Martin Weise <martin.weise@tuwien.ac.at>
2026-05-02 22:34:44 +0200

    WIP
    Signed-off-by: Martin Weise <martin.weise@tuwien.ac.at>

--- evaluation/reset.sql
+++ evaluation/reset.sql
@@ -2,5 +2,6 @@ SELECT periods.drop_system_versioning('table4');
 CREATE TABLE table4_tmp (LIKE table4 INCLUDING ALL);
 DROP TABLE table4;
 CREATE TABLE table4 (LIKE table4_tmp INCLUDING ALL);
+DROP TABLE table4_tmp;
 SELECT periods.add_system_time_period('table4', 'row_start', 'row_end');
 SELECT periods.add_system_versioning('table4');
