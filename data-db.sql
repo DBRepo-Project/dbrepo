8018aa5fb5bb64424f3c6798e0a74fec1a845d93
Martin Weise <martin.weise@tuwien.ac.at>
2026-05-03 17:11:02 +0200

    WIP
    Signed-off-by: Martin Weise <martin.weise@tuwien.ac.at>

---
+++ data-db.sql
@@ -0,0 +1,27 @@
+create table asdasd
+(
+    column00 bigint(255)  not null
+        primary key,
+    column01 varchar(255) null,
+    column02 text         null,
+    column03 bigint(255)  null,
+    column04 varchar(255) null,
+    column05 varchar(255) null,
+    column06 varchar(255) null,
+    column07 varchar(255) null,
+    column08 varchar(255) null,
+    column09 varchar(255) null,
+    column10 varchar(255) null,
+    column11 varchar(255) null,
+    column12 varchar(255) null,
+    column13 varchar(255) null,
+    column14 varchar(255) null,
+    column15 varchar(255) null,
+    column16 varchar(255) null,
+    column17 varchar(255) null,
+    column18 varchar(255) null,
+    column19 varchar(255) null,
+    column20 varchar(255) null
+)
+    with system versioning;
+
